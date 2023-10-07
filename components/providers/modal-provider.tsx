bd643bd43afa8f7e4813335a273955b8684fa560
Johnson Subedi <subede.johnson@gmail.com>
2023-10-06 20:39:21 -0400

    sidebar

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,9 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
 import { MemberModal } from "@/components/modals/members-modal";
+import { CreateChannelModal } from "@/components/modals/create-channel-modal";
+import { LeaveServerModal } from "@/components/modals/leave-server-modal";
+import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -22,6 +25,9 @@ export const ModalProvider = () => {
             <InviteModal /> 
             <EditServerModal/>
             <MemberModal/> 
+            <CreateChannelModal/> 
+            <LeaveServerModal/>
+            <DeleteServerModal/> 
         </>
     )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -59,7 +59,9 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {isModerator && (
-                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className="px-3 py-2 text-sm cursor-pointer"
+                        onClick={() => onOpen("createChannel")}
+                    >
                         Create Channel
                         <PlusCircle className='h-4 w-4 ml-auto'/>
                     </DropdownMenuItem>
@@ -68,13 +70,17 @@ export const ServerHeader = ({
                     <DropdownMenuSeparator />
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem className=" text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        onClick={() => onOpen("deleteServer", { server })}
+                    >
                         Delete Server
                         <Trash className='h-4 w-4 ml-auto'/>
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem className=" text-rose-500 px-3 py-2 text-sm cursor-pointer">
+                    <DropdownMenuItem className=" text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                        onClick={()=> onOpen("leaveServer", { server })}
+                    >
                         Leave Server
                         <LogOut className='h-4 w-4 ml-auto'/>
                     </DropdownMenuItem>
