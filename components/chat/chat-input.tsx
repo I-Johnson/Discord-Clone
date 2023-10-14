0f43bdd161d9f61ddbaa742f161f7a3c645493be
Johnson Subedi <subede.johnson@gmail.com>
2023-10-14 11:11:27 -0400

    message, files upload

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useForm } from "react-hook-form";
-import * as z from "zod";
+import * as z from 'zod';
+import axios from "axios";
+import qs from "query-string";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import {
@@ -11,7 +13,8 @@ import {
     FormItem, 
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Plus } from "lucide-react";
+import { Plus, Smile } from "lucide-react";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
     apiUrl: string;
@@ -21,8 +24,8 @@ interface ChatInputProps {
 }
 
 const formSchema = z.object({
-    content: z.string().min(1), 
-})
+    content: z.string().min(1),
+  });
 
 export const ChatInput = ({
     apiUrl, 
@@ -30,6 +33,7 @@ export const ChatInput = ({
     name, 
     type
 } : ChatInputProps) => {
+    const { onOpen } = useModal();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema), 
         defaultValues: {
@@ -38,8 +42,17 @@ export const ChatInput = ({
     });
 
     const isLoading = form.formState.isSubmitting; 
-    const onSubmit = async (value: z.infer<typeof formSchema>) =>{
-        console.log(value);
+    const onSubmit = async (values: z.infer<typeof formSchema>) =>{
+        try{
+            const url = qs.stringifyUrl({
+                url: apiUrl,
+                query,
+            });
+
+            await axios.post(url, values)
+        } catch (error){
+            console.log(error);
+        }
     }
     
     return (
@@ -54,7 +67,7 @@ export const ChatInput = ({
                                 <div className="relative p-4 pb-6">
                                     <button
                                         type="button"
-                                        onClick={() => {}}
+                                        onClick={() => onOpen("messageFile", {apiUrl, query})}
                                         className="absolute top-7 left-8 h-[24px] w-[24px]
                                         bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600
                                         dark:hover:bg-zinc-300 transition rounded-full p-1 flex
@@ -62,6 +75,17 @@ export const ChatInput = ({
                                     >
                                         <Plus className="text-white dark:text-[#313339]"/>
                                     </button>
+                                    <Input
+                                        disabled={isLoading}
+                                        className="px-14 py-6 bg-zinc-200/90 dark:bg-zinc-700/75 
+                                        border-none border-0 focus-visible:ring-0
+                                        focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                                        placeholder={`Message ${type === "conversation" ? name: "#" + name}`}
+                                        {...field}
+                                    />
+                                    <div className="absolute top-7 right-8">
+                                        <Smile/>
+                                    </div>
                                 </div>
                             </FormControl>
                         </FormItem>
