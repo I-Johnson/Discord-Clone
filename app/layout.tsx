a91b132a5c074a94394551bad84cd026dbf25cfa
Johnson Subedi <subede.johnson@gmail.com>
2023-10-18 15:15:32 -0400

    chat updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/providers/theme-providers'
 import {cn} from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { SocketProvider } from '@/components/providers/socket-provider'
+import { QueryProvider } from '@/components/providers/query-provider'
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -33,7 +34,9 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider/>
-              {children}
+              <QueryProvider>
+                {children}
+              </QueryProvider>
             </SocketProvider>
           </ThemeProvider>
           
